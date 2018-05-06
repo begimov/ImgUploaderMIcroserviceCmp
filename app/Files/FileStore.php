ca747e3e0297f834f810e2d76e4463ebc233020d
begimov <begimov@gmail.com>
2018-05-06 11:03:48 +0300

    basic storing of Image model has been implemented in FileStore class

--- app/Files/FileStore.php
+++ app/Files/FileStore.php
@@ -2,14 +2,30 @@
 
 namespace App\Files;
 
+use Exception;
 use Slim\Http\UploadedFile;
 
 class FileStore
 {
+    protected $stored = null;
+
     public function store(UploadedFile $file)
     {
-        $file->moveTo(uploads_path($file->getClientFilename()));
+        try {
+            $model = $this->createModel($file);
+
+            $file->moveTo(uploads_path($model->uuid));
+        } catch(Exception $e) {
+            //
+        }
 
         return $this;
+    }
+
+    protected function createModel(UploadedFile $file)
+    {
+        return $this->stored = Image::create([
+            'uuid' => 'sdfsdfsdf'
+        ]);
     }
 }
