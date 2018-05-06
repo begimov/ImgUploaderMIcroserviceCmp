50c952663e36eefa843dcc61cfb86a98a1fc970e
begimov <begimov@gmail.com>
2018-05-06 08:03:14 +0300

    bootstrap file has been updated to match building of an api service

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,24 +15,9 @@ $app = new Slim\App([
         'app' => [
             'name' => getenv('APP_NAME')
         ],
-
-        'views' => [
-            'cache' => getenv('VIEW_CACHE_DISABLED') === 'true' ? false : __DIR__ . '/../storage/views'
-        ]
     ],
 ]);
 
 $container = $app->getContainer();
 
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
-        'cache' => $container->settings['views']['cache']
-    ]);
-
-    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
-
-    return $view;
-};
-
-require_once __DIR__ . '/../routes/web.php';
+require_once __DIR__ . '/../routes/api.php';
